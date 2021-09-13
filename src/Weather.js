3a1ba1286d72f1938a1cbb14c04c1e328d6714d1
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-09-13 13:38:48 +1000

    imported code from CodeSandbox

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,63 @@
 import React from "react";
-import axios from "axios";
-import Loader from "react-loader-spinner";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
-  }
-
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=98269c6efc2a2e14e6b32d6e0cd4f076&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Sunny",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10,
+  };
 
   return (
-    <Loader
-      type="Puff"
-      color="yellow"
-      height={100}
-      width={100}
-      timeout={3000} //3 secs
-    />
+    <div className="Weather">
+      <form>
+        <div className="input-group mb-3">
+          <input
+            type="search"
+            className="form-control"
+            placeholder="Enter a City"
+            autoFocus="on"
+            autoComplete="off"
+          />
+          <input
+            className="btn btn-outline-secondary"
+            type="submit"
+            value="Search"
+          />
+        </div>
+      </form>
+
+      <hr className="line-dividing" />
+      <h1>{weatherData.city}</h1>
+      <div className="row">
+        <div className="col-4">
+          <ul>
+            <li> {weatherData.date} </li>
+            <li> {weatherData.description} </li>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} k/h</li>
+          </ul>
+        </div>
+
+        <div className="col-8">
+          <div className="weather-temperature">
+            <img src={weatherData.imgUrl} alt="Clear" className="icon" />
+            <span className="bold-temperature">
+              <strong>{weatherData.temperature}</strong>
+            </span>
+            <span className="units">
+              <a href="/" className="active">
+                °C{" "}
+              </a>
+              |<a href="/">°F</a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Weather from "./Weather";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <div className="App mt-3 wrapper">
+      <Weather />
+      <footer>
+        <a href="https://github.com/Lilish-Warrior/weather-app-react">
+          Open source code
+        </a>{" "}
+        by <a href="https://github.com/Lilish-Warrior">Lilish Warrior </a>
+      </footer>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
