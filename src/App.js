aa97c1bea6814440eef239ce0ebc4be7adc4421d
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-09-13 13:12:24 +1000

    Added axios to display Weather

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hello </h1>
+        <Weather city="Mexico" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=98269c6efc2a2e14e6b32d6e0cd4f076&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000} //3 secs
+    />
+  );
+}
