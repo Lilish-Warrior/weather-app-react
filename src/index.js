bbf2edfe8b432809534236776e1ef4a74805236a
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-09-14 12:22:55 +1000

    Improved css files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App mt-3 wrapper">
-      <Weather />
-      <footer>
-        <a href="https://github.com/Lilish-Warrior/weather-app-react">
-          Open source code
-        </a>{" "}
-        by <a href="https://github.com/Lilish-Warrior">Lilish Warrior </a>
-      </footer>
+    <div className="App">
+      <div className="container mt-3">
+        <Weather />
+        <footer>
+          <a href="https://github.com/Lilish-Warrior/weather-app-react">
+            Open source code
+          </a>{" "}
+          by <a href="https://github.com/Lilish-Warrior">Lilish Warrior </a>
+        </footer>
+      </div>
     </div>
   );
 }
